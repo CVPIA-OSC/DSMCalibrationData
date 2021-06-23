15cc6a3e84ff29a71bf0c97e9351799da3d254b3
sadie <sgill@flowwest.com>
2021-06-23 13:55:06 -0700

    renames data caches to include _params

--- data-raw/load_synth_years.R
+++ data-raw/load_synth_years.R
@@ -1,12 +1,12 @@
 # create the data to be cached ----
 # fall run
-fall_run_calibration <- set_synth_years(species = "fr")
-usethis::use_data(fall_run_calibration, overwrite = TRUE)
+fall_run_calibration_params <- set_synth_years(species = "fr")
+usethis::use_data(fall_run_calibration_params, overwrite = TRUE)
 
 # spring
-spring_run_calibration <- set_synth_years(species = "sr")
-usethis::use_data(spring_run_calibration, overwrite = TRUE)
+spring_run_calibration_params <- set_synth_years(species = "sr")
+usethis::use_data(spring_run_calibration_params, overwrite = TRUE)
 
 # winter
-winter_run_calibration <- set_synth_years(species = "wr")
-usethis::use_data(winter_run_calibration, overwrite = TRUE)
+winter_run_calibration_params <- set_synth_years(species = "wr")
+usethis::use_data(winter_run_calibration_params, overwrite = TRUE)
