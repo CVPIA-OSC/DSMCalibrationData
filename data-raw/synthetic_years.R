760bb213ae7861afa15ccba65c57fade88d5beb3
ErinCain <ecain@flowwest.com>
2021-06-23 08:19:57 -0700

    Adds year to calibration_year_index to have 21 total years of data

--- data-raw/synthetic_years.R
+++ data-raw/synthetic_years.R
@@ -3,24 +3,24 @@ library(waterYearType)
 library(tidyverse)
 
 target_sac <- water_year_indices %>%
-  filter(location == 'Sacramento Valley', between(WY, 1998, 2017)) %>%
+  filter(location == 'Sacramento Valley', between(WY, 1998, 2018)) %>%
   select(WY, Index)
 
 options_sac <- water_year_indices %>%
-  filter(location == 'Sacramento Valley', between(WY, 1980,1999)) %>%
+  filter(location == 'Sacramento Valley', between(WY, 1979, 2000)) %>%
   pull(Index)
 
-names(options_sac) <- 1980:1999
+names(options_sac) <- 1979:2000
 
 target_sj <- water_year_indices %>%
-  filter(location == 'San Joaquin Valley', between(WY, 1998, 2017)) %>%
+  filter(location == 'San Joaquin Valley', between(WY, 1998, 2018)) %>%
   select(WY, Index)
 
 options_sj <- water_year_indices %>%
-  filter(location == 'San Joaquin Valley', between(WY, 1980,1999)) %>%
+  filter(location == 'San Joaquin Valley', between(WY, 1979, 2000)) %>%
   pull(Index)
 
-names(options_sj) <- 1980:1999
+names(options_sj) <- 1979:2000
 
 synth_year_mapping <- map_chr(seq(target_sj$Index),
                       ~names(
@@ -30,7 +30,7 @@ synth_year_mapping <- map_chr(seq(target_sj$Index),
                         )
                     )
 
-calibration_proxy_year <- data.frame(year = 1998:2017, calibration_year = synth_year_mapping,
+calibration_proxy_year <- data.frame(year = 1998:2018, calibration_year = synth_year_mapping,
                    sac_actual = target_sac$Index,
                    sac_synth = options_sac[synth_year_mapping],
                    sj_actual = target_sj$Index,
@@ -40,6 +40,6 @@ cor(calibration_proxy_year$sj_actual, calibration_proxy_year$sj_synth)
 cor(calibration_proxy_year$sac_actual, calibration_proxy_year$sac_synth)
 
 
-calibration_year_index <- setNames(calibration_proxy_year$calibration_year, 1998:2017)
+calibration_year_index <- setNames(calibration_proxy_year$calibration_year, 1998:2018)
 usethis::use_data(calibration_year_index, overwrite = TRUE)
 
