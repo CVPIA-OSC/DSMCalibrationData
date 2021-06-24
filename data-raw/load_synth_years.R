56d88eb7bfe5af6cbb4da7e6aa80993d2dd1e5d8
sadie <sgill@flowwest.com>
2021-06-24 14:06:10 -0700

    adds to caching script ensure most uptodate packages are installed

--- data-raw/load_synth_years.R
+++ data-raw/load_synth_years.R
@@ -1,3 +1,11 @@
+remotes::install_github("CVPIA-OSC/DSMflow@main")
+remotes::install_github("CVPIA-OSC/DSMtemperature@main")
+remotes::install_github("CVPIA-OSC/DSMhabitat@main")
+remotes::install_github("CVPIA-OSC/fallRunDSM@main")
+remotes::install_github("CVPIA-OSC/winterRunDSM@main")
+remotes::install_github("CVPIA-OSC/latefallRunDSM@main")
+remotes::install_github("CVPIA-OSC/springRunDSM@main")
+
 # create the data to be cached ----
 # fall run
 fall_run_calibration_params <- set_synth_years(species = "fr")
