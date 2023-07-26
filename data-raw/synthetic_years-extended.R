129bc39b3937b5dca952184def4912b947d46785
Emanuel Rodriguez <erodriguez@flowwest.com>
2023-07-26 13:18:04 -0700

    fixes mistake of overwriting

--- data-raw/synthetic_years-extended.R
+++ data-raw/synthetic_years-extended.R
@@ -7,20 +7,20 @@ target_sac <- water_year_indices %>%
   select(WY, Index)
 
 options_sac <- water_year_indices %>%
-  filter(location == 'Sacramento Valley', between(WY, 1980, 2000)) %>%
+  filter(location == 'Sacramento Valley', between(WY, 1920, 2000)) %>%
   pull(Index)
 
-names(options_sac) <- 1980:2000
+names(options_sac) <- 1920:2000
 
 target_sj <- water_year_indices %>%
   filter(location == 'San Joaquin Valley', between(WY, 1997, 2017)) %>%
   select(WY, Index)
 
 options_sj <- water_year_indices %>%
-  filter(location == 'San Joaquin Valley', between(WY, 1980, 2000)) %>%
+  filter(location == 'San Joaquin Valley', between(WY, 1920, 2000)) %>%
   pull(Index)
 
-names(options_sj) <- 1980:2000
+names(options_sj) <- 1920:2000
 
 synth_year_mapping <- map_chr(seq(target_sj$Index),
                               ~names(
@@ -39,16 +39,16 @@ calibration_proxy_year <- data.frame(year = 1997:2017, calibration_year = synth_
 cor(calibration_proxy_year$sj_actual, calibration_proxy_year$sj_synth)
 cor(calibration_proxy_year$sac_actual, calibration_proxy_year$sac_synth)
 
-calibration_year_spawn_index <- setNames(calibration_proxy_year$calibration_year, 1997:2017)
-calibration_year_index <- calibration_year_spawn_index[-1]
+calibration_year_spawn_index_v2 <- setNames(calibration_proxy_year$calibration_year, 1997:2017)
+calibration_year_index_v2 <- calibration_year_spawn_index[-1]
 
-usethis::use_data(calibration_year_index, overwrite = TRUE)
-usethis::use_data(calibration_year_spawn_index, overwrite = TRUE)
+usethis::use_data(calibration_year_index_v2, overwrite = TRUE)
+usethis::use_data(calibration_year_spawn_index_v2, overwrite = TRUE)
 
 calibration_year_index_2019 <- c("1998" = "1998", "1999" = "1997", "2000" = "1993", "2001" = "1981", "2002" = "1989",
-           "2003" = "1993", "2004" = "1993", "2005" = "1993", "2006" = "1998", "2007" = "1994",
-           "2008" = "1988", "2009" = "1994", "2010" = "1985", "2011" = "1997", "2012" = "1985",
-           "2013" = "1994", "2014" = "1992", "2015" = "1992", "2016" = "1989", "2017" = "1998")
+                                 "2003" = "1993", "2004" = "1993", "2005" = "1993", "2006" = "1998", "2007" = "1994",
+                                 "2008" = "1988", "2009" = "1994", "2010" = "1985", "2011" = "1997", "2012" = "1985",
+                                 "2013" = "1994", "2014" = "1992", "2015" = "1992", "2016" = "1989", "2017" = "1998")
 
 calibration_year_spawn_index_2019 <- c("1997" = "1997", calibration_year_index_2019)
 
